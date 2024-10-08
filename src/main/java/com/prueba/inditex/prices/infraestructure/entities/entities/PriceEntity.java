4fabd57750981a4238802ec8562769e45522fcf6
DIR\sergio.aguilera <sergio.aguilera@accenture.com>
2024-10-08 15:49:26 +0200

    ADD - Añadimos estructura inicial, dependencias,entidad principal,las anotaciones y las cofiguraciones para JPA, además del conjunto de datos de prueba

---
+++ src/main/java/com/prueba/inditex/prices/infraestructure/entities/entities/PriceEntity.java
@@ -0,0 +1,46 @@
+package com.prueba.inditex.prices.infraestructure.entities.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "PRICE")
+@NoArgsConstructor
+@Getter
+public class PriceEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "BRAND_ID", nullable = false)
+    private Long brandId;
+
+    @Column(name = "START_DATE", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "END_DATE", nullable = false)
+    private LocalDateTime endDate;
+
+    @Column(name = "PRICE_LIST", nullable = false)
+    private Long priceList;
+
+    @Column(name = "PRODUCT_ID", nullable = false)
+    private Long productId;
+
+    @Column(name = "PRIORITY", nullable = false)
+    private Integer priority;
+
+    @Column(name = "PRICE", nullable = false)
+    private Double price;
+
+    @Column(name = "CURR", nullable = false)
+    private String currency;
+}
